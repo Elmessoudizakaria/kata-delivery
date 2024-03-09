e870bc6373cfd7d1203086a39f81de8e3bd45db0
Elmessoudizakaria <js.developer02@gmail.com>
2024-03-09 13:11:00 +0100

    initial commit

---
+++ src/test/java/com/kata/delivery/DeliveryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kata.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class DeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
